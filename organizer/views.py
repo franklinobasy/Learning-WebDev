5c3db77445e7ef37333237d8731bd6a3a6e0c158
Franklin Obasi <franklinobasy@gmail.com>
2020-11-16 06:00:12 +0100

    first commit

--- organizer/views.py
+++ organizer/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.template import Context, loader
+from django.http.response import HttpResponse
+
+from .models import Tag
+
+def homepage(request):
+    tag_list = Tag.objects.all()
+    template = loader.get_template('organizer/tag_list.html')
+    context = Context({'tag_list' : tag_list})
+    html_output = template.render(context)
+    return HttpResponse(html_output)
 
 # Create your views here.
